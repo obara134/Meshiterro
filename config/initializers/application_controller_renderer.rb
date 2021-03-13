e1e35d7829dd750df2583bfcc6e5cafe499b6cb8
obara134 <tyobara@icloud.com>
2021-03-13 20:26:50 +0000

    finish[4-3]

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5027a285e48af1b087e528993c29534c76643b1f6167c4f0813cbd290d59c6b7af191c01e97cab50c91e61578b8210641b5422125418ccab2beb7392c3421fed'
